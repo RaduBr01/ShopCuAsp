59050653fde3a7fd152aced57c3b44b7c02b8dc8
Radu Branici <braniciradu@yahoo.com>
2023-09-24 21:54:33 +0300

    Update butoane, pagina de mesaje pt admin

---
+++ Shop/Helpers/EmailSender.cs
@@ -0,0 +1,22 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Shop.Helpers
+{
+    public class EmailSender
+    {
+        public static async Task SendEmail(string toEmail,string username,string subject,string message)
+        {
+            string apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("branicir@gmail.com", "Shopppal");
+            var to = new EmailAddress(toEmail, username);
+            var plainTextContent = message;
+            var htmlContent = "";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+    
+
+        }
+    }
+}
--- Shop/Pages/Contact.cshtml.cs
+++ Shop/Pages/Contact.cshtml.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Shop.Helpers;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Shop.Pages
@@ -57,7 +60,48 @@ namespace Shop.Pages
                 ErrorMessage = "Please fill all required fields";
                 return;
             }
-            if (Phone == null) Phone = "";
+            Phone ??= ""; //compound assingment if phone== null atunci ia valoarea ""
+
+            try
+            {
+                string ConnectionString = "Data Source=.\\sqlexpress;Initial Catalog=Shop;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    string sql = "INSERT INTO messages " +
+                                 "(firstname,lastname,email,phone,subject,message) VALUES " +
+                                 "(@firstname,@lastname,@email,@phone,@subject,@message);";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.Add(new SqlParameter("@firstname", SqlDbType.NVarChar, 50)).Value = FirstName;
+                        command.Parameters.Add(new SqlParameter("@lastname", SqlDbType.NVarChar, 50)).Value = LastName;
+                        command.Parameters.Add(new SqlParameter("@email", SqlDbType.NVarChar, 100)).Value = Email;
+                        command.Parameters.Add(new SqlParameter("@phone", SqlDbType.NVarChar, 20)).Value = Phone;
+                        command.Parameters.Add(new SqlParameter("@subject", SqlDbType.NVarChar, 100)).Value = Subject;
+                        command.Parameters.Add(new SqlParameter("@message", SqlDbType.NVarChar, -1)).Value = Message; // Use -1 for max length
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+                return;
+            }
+
+            string username = FirstName + " " + LastName;
+            string EmailSubject = "Message notification";
+            string EmailMessage = "Dear " + username + ",\n" +
+                "Message received. Thank you for contacting us.\n" +
+                "We will try to reach you soon";
+            
+            
+            EmailSender.SendEmail(Email, username, EmailSubject, EmailMessage).Wait();
+ 
 
             SuccesMessage = "Message received";
 
