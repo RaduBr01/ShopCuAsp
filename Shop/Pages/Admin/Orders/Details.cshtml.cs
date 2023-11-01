1c65c6346a24372fe11bd0358c9df172d6565735
Radu Branici <braniciradu@yahoo.com>
2023-11-01 23:41:47 +0200

    Now order_ids are showing to the specific order

--- Shop/Pages/Admin/Orders/Details.cshtml.cs
+++ Shop/Pages/Admin/Orders/Details.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Drawing.Printing;
 
 namespace Shop.Pages.Admin
 {
@@ -14,23 +15,27 @@ namespace Shop.Pages.Admin
         {
             try
             {
+                string id = Request.Query["id"];
+   
                 string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=Shop;Integrated Security=True";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    string sql = "SELECT * FROM order_items";
+                    string sql = "SELECT * FROM order_items WHERE order_id=@OrderId ";
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
-                        
+                        command.Parameters.AddWithValue("@OrderId", id); // Use a parameter to avoid SQL injection
+
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
+                                Console.WriteLine("am ajuns aici");
                                 MessageInfo messageInfo = new MessageInfo();
-                                messageInfo.orderId = reader.GetInt32(1); // Adjust as per your database schema
-                                messageInfo.bookId = reader.GetInt32(2);  // Adjust as per your database schema
-                                messageInfo.quantity = reader.GetInt32(3); // Adjust as per your database schema
-                                messageInfo.unitPrice = reader.GetDecimal(4); // Adjust as per your database schema
+                                messageInfo.orderId = reader.GetInt32(0); // Assuming the order_id is in the first column
+                                messageInfo.bookId = reader.GetInt32(1);  // Assuming the book_id is in the second column
+                                messageInfo.quantity = reader.GetInt32(2); // Assuming the quantity is in the third column
+                                messageInfo.unitPrice = reader.GetInt32(3); // Assuming the unit_price is in the fourth column
                                 Messages.Add(messageInfo);
                             }
                         }
